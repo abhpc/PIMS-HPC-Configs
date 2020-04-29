b8c58d9a9ddc26c9618c8a6542fd5f10c02897a3
abhpc <37471375+abhpc@users.noreply.github.com>
2020-04-29 15:59:47 +0800

    update

--- guid-A.sh
+++ guid-A.sh
@@ -2,16 +2,16 @@
 did=`lspci |grep -i mell|awk '{print $1}'`
 imac=`mstflint -d $did q|grep -i macs|awk '{print $3}'`
 imac2=`echo $imac | sed 's/\(..\)/\1:/g'`
-d=`echo ${imac2:0:-1}`
+mac=`echo ${imac2:0:-1}`
 
-a=`ibstat|grep -i port|grep -i guid|awk '{print $3}'`
-b=`hostname`
+iguid=`ibstat|grep -i port|grep -i guid|awk '{print $3}'`
+host=`hostname`
 #echo $b $a
-c=`echo $a | sed 's/\(..\)/\1:/g'`
-f=`hostname -I`
+iguid2=`echo $iguid | sed 's/\(..\)/\1:/g'`
+ip=`hostname -I`
 
-echo -e "\t\thost $b {"
-echo -e "\t\t\t\thardware ethernet  $d;"
-echo -e "\t\t\t\toption dhcp-client-identifier =  ff:00:00:00:00:00:02:00:00:02:c9:00:${c:3:23};"
-echo -e "\t\t\t\tfixed-address $f;"
+echo -e "\t\thost $host {"
+echo -e "\t\t\t\thardware ethernet  $mac;"
+echo -e "\t\t\t\toption dhcp-client-identifier =  ff:00:00:00:00:00:02:00:00:02:c9:00:${iguid2:3:23};"
+echo -e "\t\t\t\tfixed-address $ip;"
 echo -e "\t\t}"
--- guid.sh
+++ guid.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
-a=`ibstat|grep -i port|grep -i guid|awk '{print $3}'`
-b=`hostname`
+iguid=`ibstat|grep -i port|grep -i guid|awk '{print $3}'`
+host=`hostname`
 #echo $b $a
-c=`echo $a | sed 's/\(..\)/\1:/g'`
-d=`echo ${c:3:9}`
-e=`echo ${c:${#c}-9:-1}`
-f=`hostname -I`
+iguid2=`echo $iguid | sed 's/\(..\)/\1:/g'`
+imac1=`echo ${iguid2:3:9}`
+imac2=`echo ${iguid2:${#iguid2}-9:-1}`
+ip=`hostname -I`
 
-echo -e "\t\thost $b {"
-echo -e "\t\t\t\thardware ethernet  $d$e;"
-echo -e "\t\t\t\toption dhcp-client-identifier =  ff:00:00:00:00:00:02:00:00:02:c9:00:${c:3:23};"
-echo -e "\t\t\t\tfixed-address $f;"
+echo -e "\t\thost $host {"
+echo -e "\t\t\t\thardware ethernet  $imac1$imac2;"
+echo -e "\t\t\t\toption dhcp-client-identifier =  ff:00:00:00:00:00:02:00:00:02:c9:00:${iguid2:3:23};"
+echo -e "\t\t\t\tfixed-address $ip;"
 echo -e "\t\t}"
