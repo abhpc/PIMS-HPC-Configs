3a263e5b25fd8052ac2b3eaf3fc2b723d1635849
abhpc <37471375+abhpc@users.noreply.github.com>
2020-05-09 11:18:21 +0800

    update

--- guid.sh
+++ guid.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
+did=`lspci |grep -i mell|awk '{print $1}'`
+imac=`mstflint -d $did q|grep -i macs|awk '{print $3}'`
+imac2=`echo $imac | sed 's/\(..\)/\1:/g'`
+mac=`echo ${imac2:0:-1}`
+
 iguid=`ibstat|grep -i port|grep -i guid|awk '{print $3}'`
 host=`hostname`
 #echo $b $a
 iguid2=`echo $iguid | sed 's/\(..\)/\1:/g'`
-imac1=`echo ${iguid2:3:9}`
-imac2=`echo ${iguid2:${#iguid2}-9:-1}`
 ip=`hostname -I`
 
 echo -e "\t\thost $host {"
-echo -e "\t\t\t\thardware ethernet  $imac1$imac2;"
+echo -e "\t\t\t\thardware ethernet  $mac;"
 echo -e "\t\t\t\toption dhcp-client-identifier =  ff:00:00:00:00:00:02:00:00:02:c9:00:${iguid2:3:23};"
 echo -e "\t\t\t\tfixed-address $ip;"
 echo -e "\t\t}"
